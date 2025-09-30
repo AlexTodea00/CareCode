16233194b0403e0d8a0ba2d7e065fa3ee2f1d556
alextodea <alex.todea@mer.eco>
2025-09-30 15:58:12 +0300

    Small fix

--- src/pages/landingPage/faq/FAQPage.tsx
+++ src/pages/landingPage/faq/FAQPage.tsx
@@ -15,59 +15,55 @@ export default function FAQPage({ ref }: RefType): JSX.Element {
       <p className={styles.subtitle}>
         Everything you need to know about CareCode
       </p>
-      <Accordion
-        className={`${styles.accordion} mt-10`}
-        type="single"
-        collapsible
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            How does CareCode store my information?
-          </AccordionTrigger>
-          <AccordionContent>
-            Your information is stored in secure, encrypted databases, and you
-            control exactly what information is accessible when your QR code is
-            scanned.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion className={`${styles.accordion}`} type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            How do I update my medical information?
-          </AccordionTrigger>
-          <AccordionContent>
-            You can update your information anytime through the portal app by
-            logging into your account. Changes are instantly reflected when your
-            QR code is scanned, without needing to replace your stickers or
-            products.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion className={`${styles.accordion}`} type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            Can I use CareCode for my children or elderly parents?
-          </AccordionTrigger>
-          <AccordionContent>
-            Absolutely! CareCode is perfect for children, elderly individuals,
-            or anyone who might not be able to communicate their medical needs
-            in an emergency.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion className={`${styles.accordion}`} type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            What happens if my QR code is scanned by someone other than medical
-            personnel?
-          </AccordionTrigger>
-          <AccordionContent>
-            You control what information is visible. By default, all your
-            relevant medical information is shared when the QR code is scanned
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <div className={styles['accordion-wrapper']}>
+        <Accordion
+          className={`${styles.accordion} mt-10`}
+          type="multiple"
+          defaultValue={['item-1', 'item-2', 'item-3', 'item-4']}
+        >
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              How does CareCode store my information?
+            </AccordionTrigger>
+            <AccordionContent>
+              Your information is stored in secure, encrypted databases, and you
+              control exactly what information is accessible when your QR code
+              is scanned.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>
+              How do I update my medical information?
+            </AccordionTrigger>
+            <AccordionContent>
+              You can update your information anytime through the portal app by
+              logging into your account. Changes are instantly reflected when
+              your QR code is scanned, without needing to replace your stickers
+              or products.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>
+              Can I use CareCode for my children or elderly parents?
+            </AccordionTrigger>
+            <AccordionContent>
+              Absolutely! CareCode is perfect for children, elderly individuals,
+              or anyone who might not be able to communicate their medical needs
+              in an emergency.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger>
+              What happens if my QR code is scanned by someone other than
+              medical personnel?
+            </AccordionTrigger>
+            <AccordionContent>
+              You control what information is visible. By default, all your
+              relevant medical information is shared when the QR code is scanned
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
       <span className="mt-10 ">
         Still have questions?
         <a className={styles.link} href="mailto:alextodea14@yahoo.ro">
